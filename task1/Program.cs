2eae966c84215babea601257241023f0a7504110
geekbrains <maksimov0904@icloud.com>
2023-01-27 01:19:40 +0500

    Вычисление квадрата

--- task1/Program.cs
+++ task1/Program.cs
@@ -4,17 +4,19 @@
 // a = 9, b = -3 -> да 
 // a = -3
 
-Console.Clear
-Console.Write("введите число 1:");
+Console.Clear;
+
+Console.Write(  "  введите число 1:");
 int num1 = int.Parse(Console.ReadLine());
 Console.Write("введите число 2:");
 int num2 = int.Parse(Console.ReadLine());
 int num2sqr =num1*num2;
 if (num2sqr==num1)
 {
-    console.WriteLine("первое число является квадратом второго");
+    Console.WriteLine("первое число является квадратом второго");
 }
 else
 {
-    console.WriteLine("первое число не является квадратом второго");
+    Console.WriteLine("первое число не является квадратом второго");
 }
+
