46f85c289a4b74fe97a0afc4cf510e668327148d
geekbrains <maksimov0904@icloud.com>
2023-01-27 22:00:52 +0500

    Add a day of week

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// 
+Console.WriteLine("Введите число от 1 до 7");
+int day = int.Parse(Console.ReadLine());
+if (day==1)
+{Console.WriteLine("Monday");}
+if (day==2)
+{Console.WriteLine("Thuersday");}
+if (day==3)
+{Console.WriteLine("Wensday");}
+if (day==4)
+{Console.WriteLine("Thirday");}
+if (day==5)
+{Console.WriteLine("Friday");}
+if (day==6)
+{Console.WriteLine("Saturday");}
+if (day==7)
+{Console.WriteLine("Sunday");}
+if (day>7)
+{Console.WriteLine("ERROR");}
