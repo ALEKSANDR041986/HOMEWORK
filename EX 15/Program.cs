cce8d33df63a5064579506fbfbd46b8fc444466c
geekbrains <maksimov0904@icloud.com>
2023-01-31 23:41:19 +0500

    Add Exersice 15

---
+++ EX 15/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+Console.Clear();
+int day = new Random().Next(1,8);
+if(day<6)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"{day}->НЕТ");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"{day}->ДА");
+}
