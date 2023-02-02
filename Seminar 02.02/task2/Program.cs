13f8ec628b3745c2d2fda3e16ead4eac03b29caf
geekbrains <maksimov0904@icloud.com>
2023-02-02 22:41:19 +0500

    Add task2

---
+++ Seminar 02.02/task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая по 
+// заданному номеру четверти, показывает 
+// диапазон возможных координат точек в этой 
+// четверти (x и y).
+
+Console.WriteLine("Введите номер четверти");
+int QUA = int.Parse(Console.ReadLine());
+
+if (QUA==1)
+{
+    Console.WriteLine("X>0,Y>0");
+}
+else if (QUA==2)
+{
+    Console.WriteLine("X<0,Y>0");
+}
+else if (QUA==3)
+{
+    Console.WriteLine("X<0,Y<0");
+}
+else if (QUA==4)
+{
+    Console.WriteLine("X>0,Y<0");
+}
+else 
+
+{
+    Console.WriteLine("ошибка");
+}
