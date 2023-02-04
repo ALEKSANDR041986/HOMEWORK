708dbc7d2e1167530f3f5d21f8ec4e65339145f3
geekbrains <maksimov0904@icloud.com>
2023-02-04 23:00:47 +0500

    add EX23

---
+++ Ex23/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("введите число N ");
+int N = int.Parse(Console.ReadLine());
+int x=0;
+Console.Write($"{N}->");
+if (N>0)
+{
+while (x<N)
+{
+Console.Write(Math.Pow(x,3)+", ");
+x++;
+}
+}
+else
+while (x>N)
+{
+Console.Write(Math.Pow(x,3)+", ");
+x--;
+}
+Console.Write(Math.Pow(N,3));
