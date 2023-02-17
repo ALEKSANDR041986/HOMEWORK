1b1ac422bbc2178683f643bffaf81d5af75b612b
geekbrains <maksimov0904@icloud.com>
2023-02-17 23:41:31 +0500

    add Ex 38

--- task38/Program.cs
+++ task38/Program.cs
@@ -4,8 +4,8 @@
 
 //-------- Инициализация
 
-double min = 1;
-double max = 100;
+int min = 1;
+int max = 100;
 Console.Write("Введите размер массива: ");
 int size = int.Parse(Console.ReadLine());
 double[] array = CreateArray(size, min, max);
@@ -15,12 +15,13 @@ PrintArray(array);
 Console.WriteLine($"Разница между максимальным и минимальным  элементов массива составляет {DiffArr(array)}.");
 
 //----------Создаем массив
-double[] CreateArray(int size, double min, double max)
+double[] CreateArray(int siz, int minValue, int maxValue)
 {
-    double[] res = new double[size];
-    for (double i = 0; i < size; i++)
+    double[] res = new double[siz];
+    for (int i = 0; i < siz; i++)
     {
-        res[i] = new Random().Next(min,max);
+        res[i] = new Random().NextDouble() * 10 + new Random().Next(minValue, maxValue);
+        // }            new Random().NextDouble() * 10 + new Random().Next(minValue, maxValue)  
     }
     return res;
 }
@@ -39,25 +40,30 @@ void PrintArray(double[] SomeArray)
 //------------разница в массиве
 double DiffArr(double[] SomeArray)
 {
+    double diff = 0;
     double maxnum = SomeArray[0];
     double minnum = SomeArray[0];
-    for (int i = 1; i < SomeArray.Length-1; i++)
+    for (int i = 0; i < SomeArray.Length; i++)
     {
         if (SomeArray[i] > maxnum)
         {
             maxnum = SomeArray[i];
         }
-        else
+        else if (SomeArray[i] < minnum)
         {
             minnum = SomeArray[i];
         }
+        diff = maxnum - minnum;
     }
-    double diff;
-    return maxnum;
-    return minnum;
-    diff = maxnum - minnum;
-
+    return diff;
 }
+
+
+
+
+
+
+
 
 // //----------работа
 // PrintArray(array);
